ecfbb3adc748d4ec867085b24d83043dc5a003a9
Brent Reynolds <heyheyitsbrent@gmail.com>
2021-10-13 19:06:20 -0700

    Initial conncetion to cosmos

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using MatBlazor;
+using InventoryApp.Database;
 
 namespace InventoryApp
 {
@@ -20,9 +21,31 @@ namespace InventoryApp
 
             builder.Services
                 .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+                .AddSingleton<ICosmosDbService>(
+                    await InitializeCosmosClientInstanceAsync(builder.Configuration.GetSection("CosmosDb"))
+                )
                 .AddMatBlazor();
             
+            
             await builder.Build().RunAsync();
+        }
+
+        /// <summary>
+        /// Creates a Cosmos DB database and a container with the specified partition key. 
+        /// </summary>
+        /// <returns></returns>
+        private static async Task<CosmosDbService> InitializeCosmosClientInstanceAsync(IConfigurationSection configurationSection)
+        {
+            var databaseName = configurationSection.GetSection("DatabaseName").Value;
+            var containerName = configurationSection.GetSection("ContainerName").Value;
+            var account = configurationSection.GetSection("Account").Value;
+            var key = configurationSection.GetSection("Key").Value;
+            var client = new Microsoft.Azure.Cosmos.CosmosClient(account, key);
+            var cosmosDbService = new CosmosDbService(client, databaseName, containerName);
+            // var database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
+            // await database.Database.CreateContainerIfNotExistsAsync(containerName, "/id");
+
+            return cosmosDbService;
         }
     }
 }
